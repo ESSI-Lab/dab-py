4fb7dec024cf8636955d0288157a1b3cff9e8502
alun-putra <alun.putra@edu.unifi.it>
2025-12-05 15:16:09 +0100

    Change to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dab_py",
-    version="0.2.1",
+    version="0.3.0",
     packages=find_packages(),
     install_requires=[
         "requests",
