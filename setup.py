6305e70133ccb528e38dfa0fe3a14f651556896d
Enrico Boldrini <enrico.boldrini@cnr.it>
2025-12-19 11:53:49 +0100

    modified license

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         "pandas",
         "matplotlib"
     ],
-    license="GPL-3.0",
+    license="AGPL-3.0",
     author="Alun Sagara Putra (CNR Internship)",
     description="A Python client for DAB Terms API and WHOS API (v2)",
     long_description=open("README.md", encoding="utf-8").read(),
@@ -17,7 +17,7 @@ setup(
     url="https://github.com/ESSI-Lab/dab-py/tree/main",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
